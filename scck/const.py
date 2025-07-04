29f3f6f86e8bb3af78f3ddd4414f1dcc02653ada
See.Looooo <siumabon123@gmail.com>
2025-07-04 18:02:58 +0800

    change title

--- scck/const.py
+++ scck/const.py
@@ -5,8 +5,8 @@ cmdlen = 48
 title = "\n ".join([
          ".------..------..------..------.".center(cmdlen, " "),
          "|S.--. ||C.--. ||C.--. ||K.--. |".center(cmdlen, " "),
-         "| :/\\: || :/\\: || :/\\: || :/\\: |".center(cmdlen, " "),
-         "| :\\/: || :\\/: || :\\/: || :\\/: |".center(cmdlen, " "),
+         "| :♥ : || :♦ : || :♣ : || :♠ : |".center(cmdlen, " "),
+         "| :  : || :  : || :  : || :  : |".center(cmdlen, " "),
          "| '--'S|| '--'C|| '--'C|| '--'K|".center(cmdlen, " "),
          "`------'`------'`------'`------'".center(cmdlen, " "),
          "CLI Tool on Super Computer".center(cmdlen, " "),
